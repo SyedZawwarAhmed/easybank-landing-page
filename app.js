e38fba869de3869548c4bdd4932683bff74053df
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-02-20 16:31:35 +0500

    changes made

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ const colorUnchange = (id) => {
   document.getElementById(id + "-2").style.display = "none";
   document.getElementById(id).style.display = "unset";
 };
+
+// THIS SCRIPT IS FOR THE FUNCTIONALITY OF HAMBURGER
+
+const hamburger = document.getElementById("hamburger");
+const mobileNav = document.getElementById("mobile-nav");
+let menuOpen = false;
+
+hamburger.addEventListener("click", () => {
+  mobileNav.style.transform = !menuOpen
+    ? "translate(-50%, 0)"
+    : "translate(-50%, -150%)";
+  menuOpen = !menuOpen ? true : false;
+});
