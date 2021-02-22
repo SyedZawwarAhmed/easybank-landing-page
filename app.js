048cf308f741736917fdaab88e92dc0436ff663e
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-02-22 19:50:15 +0500

    changes made

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// THIS SCRIPT IS FOR THE HOVER EFFECTS ON SOCIAL ICONS IN THE FOOTER
-
-const colorChange = (id) => {
-  document.getElementById(id).style.display = "none";
-  document.getElementById(id + "-2").style.display = "unset";
-};
-
-const colorUnchange = (id) => {
-  document.getElementById(id + "-2").style.display = "none";
-  document.getElementById(id).style.display = "unset";
-};
-
 // THIS SCRIPT IS FOR THE FUNCTIONALITY OF HAMBURGER MENU
 
 const container = document.getElementById("container");
